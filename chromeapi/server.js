d7e3de054373bc56530a1205436f4064e7fa8179
Oluwatimilehin Alabi <alabialvin@gmail.com>
2023-08-23 16:02:41 +0100

    Modified: mongo connection

--- chromeapi/server.js
+++ chromeapi/server.js
@@ -4,21 +4,15 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
-
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("DB connection successful!"));
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB connection successful!"));
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
+	console.log(`App running on port ${port}....`);
 });
